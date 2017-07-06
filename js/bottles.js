fad8a568069edb37a54eaf60d913baa47b8ea253
Anna Smith <annamsmith2893@gmail.com>
2017-07-06 17:57:15 -0500

    js file finally worksgit add -A

--- js/bottles.js
+++ js/bottles.js
@@ -1,20 +1,16 @@
-var bottles = document.getElementbyId("beersong");
-for (counter = 99; counter >= 1; counter = counter - 1) 
-{
-    if (counter == 1) {
-        bottles = 'bottle';
-    } else {
-        bottles = 'bottles';
-    }
-    console.log(counter+" "+bottles+" of beer on the wall.");
-    if (counter < 99) {
-        console.log("");
-        console.log(counter+" "+bottles+" of beer on the wall.");
-    }
-    console.log(counter+" "+bottles+" of beer.");
-    console.log("Take one down.");
-    console.log("Pass it around.");
-    if (counter == 1) {
-        console.log("No bottles of beer on the wall.");
-    }
+// copied script from matopher on github, friend in okcoders class; just using to try to learn js
+var bottles = document.getElementById("beersong");
+
+var drink = "beer";
+
+var lyrics = "";
+
+var bottles = "99";
+
+for (var i = bottles; i > 0; i--){
+    
+    lyrics = i + " bottles of " + drink + " on the wall, " + drink + 
+    ", take one down, pass it around, " + (i - 1) + " bottles of " + drink + " on the wall"
+
+    beersong.innerHTML += lyrics + "<br/>";
 }
